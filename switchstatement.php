d548543e2cb68ffdf645d21bdffc607824ebbc3f
jesicayeu <119467018+jesicayeu@users.noreply.github.com>
2023-03-03 03:42:27 -0800

    Add files via upload

---
+++ switchstatement.php
@@ -0,0 +1,25 @@
+<?php
+
+$harga = "E";
+
+switch ($harga) {
+    case 'A':
+    echo "Rp.150.000" . PHP_EOL;
+    break;
+
+    case 'B':
+    echo "Rp.250.000" . PHP_EOL;
+    break;
+
+    case 'C':
+    echo "Rp.350.000" . PHP_EOL;
+    break;
+
+    case 'D':
+    echo "Rp.450.000" . PHP_EOL;
+    break;
+
+    case 'E':
+    echo "Rp.550.000" . PHP_EOL;
+    break;
+}
---
+++ ternaryoperator.php
@@ -0,0 +1,5 @@
+<?php
+
+$bilangan = 45;
+
+echo $bilangan % 2 === 0 ? "bilangan genap" : "bilangan ganjil";
